a9cea4079ebbc5599b4f9abd80368b99e66b9fbe
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-22 10:08:25 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,8 +25,9 @@ end
 
 def return_string_array(array)
   count = 0
+  new_array = []
   while count < array.length 
- array[count] = array[count].to_s
+ new_array = array[count].to_s
     count += 1
   end
   array
