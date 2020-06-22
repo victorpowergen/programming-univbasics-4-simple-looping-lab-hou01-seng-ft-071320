684ef0abce51facec454497c80cc0479967b2f93
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-22 09:11:16 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,16 @@
-# Write your methods here
+
+def loop_message_five_times(message, num = 5)
+  count = 0
+  while count < 5 
+  puts  message
+  count += 1
+end
+end
+
+def loop_message_n_times(message, number)
+  count = 0
+  while count < 5 
+  puts  #{message} #{number}
+  count += 1
+end
+end
