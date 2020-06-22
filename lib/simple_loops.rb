b84b81e594a296d7686930503befdcaf086bea54
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-22 09:54:36 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,3 +14,11 @@ def loop_message_n_times(message, number)
   count += 1
 end
 end
+
+def output_array(array)
+  count = 0
+  while count < array.length 
+    puts array[count]
+    count += 1
+  end
+end
